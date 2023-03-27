0e27b76e5f161ea1e465d46b471962105e419183
Adriano <adriano_alves_2008@hotmail.com>
2023-03-27 13:40:22 -0300

    finalizando estilo da página sacola

--- src/pages/Sacola/index.jsx
+++ src/pages/Sacola/index.jsx
@@ -21,9 +21,30 @@ function Sacola() {
               alt={item.product.name}
             />
             <h3>{item.product.name}</h3>
-            <h4>R$ {item.product.priceSpecification.price.toFixed(2)}</h4>
+            <div className="precos">
+              <h4>R$ {item.product.priceSpecification.price.toFixed(2)}</h4>
+            </div>
           </div>
         ))}
+      </div>
+      <div className="resumo">
+        <div>
+          <p>Produtos: (3 itens)</p>
+          <p>R$ {produto.subTotal.toFixed(2)}</p>
+        </div>
+        <div>
+          <p>Frete:</p>
+          <p>R$ {produto.shippingTotal.toFixed(2)}</p>
+        </div>
+        <div>
+          <p>Desconto:</p>
+          <h4>R$ {produto.discount.toFixed(2)}</h4>
+        </div>
+        <div>
+          <h3>Subtotal</h3>
+          <h3>R$ {produto.total.toFixed(2)}</h3>
+        </div>
+        <button>Seguir para o pagamento</button>
       </div>
     </div>
   )
