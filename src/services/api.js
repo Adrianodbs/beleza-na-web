2d00c2b509a6914a143723dff90d91d0684dde77
Adriano <adriano_alves_2008@hotmail.com>
2023-04-03 19:00:56 -0300

    correção API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://www.mocky.io/v2/5b15c4923100004a006f3c07'
+const baseUrl = 'https://run.mocky.io/v3/d6e9a93f-9741-4494-b81e-637a8e9b8ddd'
 
 const apiData = axios.create({
   baseURL: baseUrl
